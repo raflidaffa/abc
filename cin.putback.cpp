a88a7bdded72583655ba8940434ce4ebdd4ff1bc
raflidaffa <112566735+raflidaffa@users.noreply.github.com>
2023-04-12 22:06:08 +0700

    Add files via upload

---
+++ cin.putback.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char karakter;
+	while (cin.get(karakter)){
+		if(karakter == 'a')
+			cin.putback('x');
+		else
+			cout << karakter;
+	}
+}
